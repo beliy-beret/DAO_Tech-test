6cc8c726c76776e48d59fa955eb906bad07c5246
AlucarD <beliy_beret@mail.ru>
2021-06-16 20:52:38 +0300

    настройка Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import reducer from "./reducer";
 const { createStore } = require("redux");
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
